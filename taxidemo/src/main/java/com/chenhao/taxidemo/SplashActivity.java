4a6dfcb5c2611fa3bfb4250e8958379b88bfb448
chenhao <ace_haochen@163.com>
2017-11-06 18:50:59 +0800

    闪屏页

---
+++ taxidemo/src/main/java/com/chenhao/taxidemo/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.chenhao.taxidemo;
+
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+/**
+ * @Description: app启动页
+ * @Author: chenhao
+ * @Time: 2017/11/6 17:16
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Log.d("SplashActivity", "   执行了");
+            ImageView imageView = (ImageView) findViewById(R.id.logo);
+            AnimatedVectorDrawable vectorDrawable = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.anim);
+            imageView.setImageDrawable(vectorDrawable);
+            vectorDrawable.start();
+       }
+    }
+}
